f392930bee580efe38f82ebf328206005ee9cc96
Abdirahman <dubowhamaan@gmail.com>
2022-01-29 11:53:17 +0300

    add article.py functions

---
+++ app/models/articles.py
@@ -0,0 +1,11 @@
+class Article:
+    '''
+   Article class to define article objects
+    '''
+    def __init__(self,image,title,author,description,time,url):
+        self.image = image
+        self.title = title
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
