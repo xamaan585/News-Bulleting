041baf76a81c7b659b402fbf1fe3033dbb30f7e4
Abdirahman <dubowhamaan@gmail.com>
2022-01-31 09:33:49 +0300

    updating navbar.html

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,3 @@
-from flask import Blueprint
 main = Blueprint('main',__name__)
+from flask import Blueprint
 from .import views,error
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-export NEWS_API_KEY =b4c50aed4f83405e844d5c99411fb9ee
+export NEWS_API_KEY=b4c50aed4f83405e844d5c99411fb9ee
 
 python3 manage.py server
